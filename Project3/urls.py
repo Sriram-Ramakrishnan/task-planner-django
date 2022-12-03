8212650484eb8724805268e8811600883f52b6ea
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-03 17:48:07 +0530

    Created Login page

--- Project3/urls.py
+++ Project3/urls.py
@@ -22,6 +22,8 @@ urlpatterns = [
     #Auth 
     path('signup/', views.signupuser, name='signupuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
+
     #Task planner
     path('home/', views.home, name='home'),
 ]
--- taskplanner/views.py
+++ taskplanner/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
 from django.contrib.auth import login,logout
 
 # Create your views here.
 def signupuser(request):
-    data = {'form': UserCreationForm}
+    data = {}
     if (request.method == 'GET'):
         # User is visiting the page
         return render(request, 'taskplanner/signupuser.html', data)
@@ -37,3 +37,24 @@ def logoutuser(request):
         logout(request)
         return redirect('home')
 
+def loginuser(request):
+    data = {'form': AuthenticationForm }
+    if (request.method == 'GET'):
+        # User is visiting the page
+        return render(request, 'taskplanner/loginuser.html', data)
+    else:
+        # User clicks on submit => POST => Create User
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(request.POST['username'],request.POST['password1'])
+                user.save()
+                # Log them after signing up
+                login(request,user)
+                return redirect('home')
+            except IntegrityError:
+                data['error'] = "User already Exists. Choose a different username"
+                return render(request, 'taskplanner/signupuser.html', data)
+
+        else:
+            data['error'] = "Passwords did not match"
+            return render(request, 'taskplanner/signupuser.html', data)
