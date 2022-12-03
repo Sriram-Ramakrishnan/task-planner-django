ef025491a896102c0bb925417c7b52019666c771
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-03 21:02:26 +0530

    Added Auth functionality with static js

--- taskplanner/views.py
+++ taskplanner/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.db import IntegrityError
-from django.contrib.auth import login,logout
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 def signupuser(request):
@@ -38,23 +38,15 @@ def logoutuser(request):
         return redirect('home')
 
 def loginuser(request):
-    data = {'form': AuthenticationForm }
-    if (request.method == 'GET'):
-        # User is visiting the page
-        return render(request, 'taskplanner/loginuser.html', data)
+    if request.method == 'GET':
+        return render(request, 'taskplanner/loginuser.html', {'form':AuthenticationForm()})
     else:
-        # User clicks on submit => POST => Create User
-        if request.POST['password1'] == request.POST['password2']:
-            try:
-                user = User.objects.create_user(request.POST['username'],request.POST['password1'])
-                user.save()
-                # Log them after signing up
-                login(request,user)
-                return redirect('home')
-            except IntegrityError:
-                data['error'] = "User already Exists. Choose a different username"
-                return render(request, 'taskplanner/signupuser.html', data)
+        username = request.POST['username'].strip()
+        password = request.POST['password'].strip()
+        user = authenticate(username=username, password=password)
 
+        if user is not None:
+            login(request, user)
+            return redirect('home')
         else:
-            data['error'] = "Passwords did not match"
-            return render(request, 'taskplanner/signupuser.html', data)
+            return render(request, 'taskplanner/loginuser.html', {'form':AuthenticationForm(), 'error': username })
