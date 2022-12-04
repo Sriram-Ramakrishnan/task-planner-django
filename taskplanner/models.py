552638c36874ce2cf3a0700d05a3964845120e4d
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-04 23:39:18 +0530

    Created model 'Task' in models.py

--- taskplanner/models.py
+++ taskplanner/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    created = models.DateField(auto_now_add=True)
+    deadline = models.DateField()
+    time_req = models.TimeField(null=True,blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+# -----------Viewing the title in DjangoAdmin-------------
+    def __str__(self):
+        return self.title
