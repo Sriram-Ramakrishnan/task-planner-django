a4b732c13726fae12fd62b3c224b7b6b83b5ed66
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-07 21:37:17 +0530

    Added Responsiveness for other parts

--- taskplanner/static/taskplanner/index.js
+++ taskplanner/static/taskplanner/index.js
@@ -4,22 +4,24 @@
 document.addEventListener("DOMContentLoaded", ()=>{
     // Sign In and Sign Up toggle 
     // Sign In and Sign Up toggle 
-const signupform = document.querySelector('.signupform');
-const loginform = document.querySelector('.loginform');
-var signup =document.getElementById("signupform");
-var login = document.getElementById("loginform");
-
-signup.onclick = function(){
-    signupform.style["display"] = "flex";
-    loginform.style["display"] = "none";
-    signup.className = "btn btn-info";
-    login.className = "btn btn-secondary";
+    const signupform = document.querySelector('.signupform');
+    const loginform = document.querySelector('.loginform');
+    var signup =document.getElementById("signupform");
+    var login = document.getElementById("loginform");
+    if (!signup&&login) { 
+        signup.onclick = function(){
+            signupform.style["display"] = "flex";
+            loginform.style["display"] = "none";
+            signup.className = "btn btn-info";
+            login.className = "btn btn-secondary";
+            }
+            
+        login.onclick = function(){
+            signupform.style["display"] = "none";
+            loginform.style["display"] = "flex";
+            signup.className = "btn btn-secondary";
+            login.className = "btn btn-info";
+        }   
     }
-    
-login.onclick = function(){
-    signupform.style["display"] = "none";
-    loginform.style["display"] = "flex";
-    signup.className = "btn btn-secondary";
-    login.className = "btn btn-info";
-}   
-})
+    }
+)
