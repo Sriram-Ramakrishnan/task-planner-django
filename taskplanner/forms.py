0808496ac87683026d958788e669ff756daaa090
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-08 20:16:42 +0530

    Fixed view of task and fixed TypeError in form

--- taskplanner/forms.py
+++ taskplanner/forms.py
@@ -4,3 +4,13 @@ class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title','description','deadline','do_date','start_time','end_time']
+    
+    def __init__(self,*args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            data = {
+                "class": "form-control"
+            }
+            self.fields[str(field)].widget.attrs.update(
+                data
+            )
--- taskplanner/models.py
+++ taskplanner/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.timezone import now
+from django import forms
 
 # Create your models here.
 
