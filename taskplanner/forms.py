a39065030720b683a075155e18a9b8d3d5bfa456
Sriram Ramakrishnan <srirampro107@gmail.com>
2022-12-09 10:03:33 +0530

    Added Widgets to Input

--- taskplanner/forms.py
+++ taskplanner/forms.py
@@ -1,6 +1,11 @@
 from django.forms import ModelForm
+from django import forms
 from .models import Task
 class TaskForm(ModelForm):
+    deadline = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    do_date = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    start_time = forms.TimeField(widget=forms.TextInput(attrs={'type':'time'}))
+    end_time = forms.TimeField(widget=forms.TextInput(attrs={'type':'time'}))
     class Meta:
         model = Task
         fields = ['title','description','deadline','do_date','start_time','end_time']
